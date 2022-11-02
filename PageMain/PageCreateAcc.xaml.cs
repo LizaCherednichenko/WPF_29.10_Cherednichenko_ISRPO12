ffe48e385b24d2e19509a1e69fb1205bcdb8ee7f
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-11-02 13:45:52 +1000

    регистрация проходит

--- PageMain/PageCreateAcc.xaml.cs
+++ PageMain/PageCreateAcc.xaml.cs
@@ -46,5 +46,36 @@ namespace WPF_29._10_Cherednichenko_ISRPO12.PageMain
                 psbPass.BorderBrush = Brushes.Green;
             }
         }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            //проверка есть ли такой же пользователь
+            if (AppConnect.model0db.User.Count(x => x.login == txbLogin.Text) > 0)
+            {
+                MessageBox.Show("Пользователь с таким логином есть!",
+                    "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            //если прошли проверку логина, записываем нового пользователя с ролью 2
+            try
+            {
+                User userObj = new User()
+                {
+                    login = txbLogin.Text, //получаю данные логина
+                    name = txbName.Text, //получаю данные имя пользователя
+                    password = txbPass.Text, //получаю пароль
+                    IdRole = 2 //присваиваю роль 2, т.к. 1 - администратор
+                };
+                AppConnect.model0db.User.Add(userObj); //добавление объекта
+                AppConnect.model0db.SaveChanges(); //сохранение
+                MessageBox.Show("Данные успешно добавлены!",
+                    "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка при добавлении данных!",
+                    "Уведомление", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
